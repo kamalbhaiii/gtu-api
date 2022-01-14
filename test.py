b31ac53147b055f42ba8104d3a1af26270142cc9
Kamal040102 <kamal5201ks@gmail.com>
2022-01-14 13:46:02 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-date = datetime.datetime.now()
-todaysDate = [date.strftime(f'%d-%B-%Y').split('-')]
-# print(todaysDate[0][1][0:3])
-todaysDate = f"{todaysDate[0][0]}-{todaysDate[0][1][0:3]}-{todaysDate[0][2]}"
-print(todaysDate)
